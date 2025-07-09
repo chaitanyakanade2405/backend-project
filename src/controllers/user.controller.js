e6944837d11694b08050ec9690c75bb578d70ee5
chaitanyakanade2405 <chaitanyakanade2405@gmail.com>
2025-07-10 02:06:09 +0530

    Debugging using Postman - fixed register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,27 +18,37 @@ const registerUser = asyncHandler(async (req, res) => {
     const { fullname, email, username, password } = req.body;
     console.log("email:", email);
 
-    if ([fullname, email, username, password].some((field) => field?.trim() === '')) {
-        throw new ApiError(400, "All fields are required");
-    }
+    if (!fullname || !email || !username || !password ||
+    [fullname, email, username, password].some(field => typeof field !== "string" || field.trim() === "")
+) {
+    throw new ApiError(400, "All fields are required and must be valid strings");
+}
+
     
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ email }, { username }] 
     })
 
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists");
     }   
-
+   // console.log(req.files);
+    
     const avatarLocalPath =  req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path; 
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required");
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    const coverImage = coverImageLocalPath ? await uploadOnCloudinary(coverImageLocalPath): { url: "" };
+
 
     if (!avatar) {
         throw new ApiError(400, "Avatar is required");
@@ -47,7 +57,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({
         fullname,
         avatar: avatar.url,
-        coverImage: coverImage.url || "",
+        coverImage: coverImage.url,
         email,
         password,
         username: username.toLowerCase(),
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("File uploaded to Cloudinary:", response.url);
+        //console.log("File uploaded to Cloudinary:", response.url);
+        fs.unlinkSync(localFilePath); // Delete the file after upload
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath); // Delete the file if upload fails
